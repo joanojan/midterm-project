0e5e06ef3e8861892d5095a9523a252010beceee
Joan Vila Valls <joan.o.jan@gmail.com>
2024-02-14 23:27:45 +0100

    basic folder structure, jQuery load common html

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(function(){
+    $("#header").load("./common/navbar.html");
+    $("#projects").load("./common/projects.html");
+    $("#cta").load("./common/cta.html");
+    $("#footer").load("./common/footer.html");
+  });
