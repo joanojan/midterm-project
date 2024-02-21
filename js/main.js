661c8698f0d9bce711075cca469727e1b6b73726
Joan Vila Valls <joan.o.jan@gmail.com>
2024-02-21 18:39:45 +0100

    cta means 'call to action'

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 $(function(){
+
     $("#nav-container").load("./common/navbar.html");
+
     $(".projects").first().load("./common/projects.html", function(){
       //TO-DO: CHANGE THE CONTENT OF THE PROJECT.HTML
     });
+
     $(".cta").load("./common/cta.html");
+
     $(".footer").load("./common/footer.html");
+
     //Services section
     $(".projects").last().load("./common/projects.html", function(){
       $(".services .projects-container .projects-title").text("Our Services");
       $("section.services ul li").removeClass(".project-li").addClass("services-li");
     });
+    
   });
