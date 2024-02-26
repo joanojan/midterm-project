95152d79b3049c49dd8c7d29aef7e9954c9f98ec
Joan Vila Valls <joan.o.jan@gmail.com>
2024-02-26 13:05:46 +0100

    mobile navbar style ---- Bug to fix: class=project-title

--- js/project.js
+++ js/project.js
@@ -4,38 +4,53 @@ import { getProjects, errorCallback } from "./utils.js";
  * load the project - and other projects
  */
 
-//other projects
-$(function () {
-  $(".projects").load("./common/projects.html", function () {
-    $(".projects ul div").removeClass("icon-wrapper");
-    $(".projects-title").text("Other Projects");
-  });
-});
 
 //get and render the first project
 getProjects().then(function (projects) {
   try {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
-    const id = urlParams.get('id');
-    const firstProjectOjbect = projects.filter(
+    const id = urlParams.get("id");
+    const projectOjbect = projects.filter(
       (project) => project.uuid === id
     );
-    const firstProject = firstProjectOjbect[0];
-    renderProject(firstProject);
-    console.log(firstProject);
+    const project = projectOjbect[0];
+    renderProject(project);
+    console.log(project);
   } catch (error) {
     errorCallback(error);
   }
 });
 
 function renderProject(project) {
-  //using vanilla javascript
   const title = document.querySelector(".project-title");
   title.textContent = project.name;
-  //or using jQuery
   $(".project-article-subject").text(project.description);
   $(".project-dateTime").text(project.completed_on);
   $(".project-article-image").attr("src", project.image);
   $(".project-article-text").html(project.content);
 }
+
+//other projects
+$(function () {
+  $(".projects").load("./common/projects.html", function () {
+    $(".projects ul div").removeClass("icon-wrapper");
+    $(".projects-title").text("Other Projects");
+    $(".projects-container img").each(function (index) {
+      if (index === 0) $(this).attr("src", "./assets/projects-section/4.jpg");
+      if (index === 1) $(this).attr("src", "./assets/projects-section/5.jpg");
+      if (index === 2) $(this).attr("src", "./assets/projects-section/6.jpg");
+    });
+    $(".project-title").each(function (index) {
+      if (index === 0) $(this).text("Orbit");
+      if (index === 1) $(this).text("Purify");
+      if (index === 2) $(this).text("CriptoPie");
+    });
+    $(".project-subject").each(function (index) {
+      if (index === 0) $(this).text("UI Design");
+      if (index === 1) $(this).text("UI/UX Design");
+      if (index === 2) $(this).text("App Development");
+    });
+    $(".project-learn-more[href]").attr("href", "404.html");
+  });
+});
