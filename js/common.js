4c5cb197e4d123bd92ae2d5614e5067aa749d0ee
Joan Vila Valls <joan.o.jan@gmail.com>
2024-02-29 11:36:14 +0100

    toggle menu + js toggle

--- js/common.js
+++ js/common.js
@@ -3,7 +3,6 @@
  *
 */
 
-
 $(function () {
   //navbar is fixed in top of each page ->
   $("#nav-container").load("./common/navbar.html", function () {
@@ -11,6 +10,8 @@ $(function () {
     if(window.location.pathname === '/contact.html') {
       $("#contact-button").remove();
     }
+    //mobile: show menu onclick.
+    toggleMenu();
   });
 
   //the "Call To Action" is a form to get the email of the user.
@@ -19,4 +20,16 @@ $(function () {
   //footer
   $(".footer").load("./common/footer.html");
 });
-/**================================================= */
+
+/**
+ * 
+ *        Toggle menu: show hamburguer menu on mobile and 
+ *      display the links when the user clicks on it.
+ * 
+ */
+
+function toggleMenu() {
+  $("#toggle-btn").on("click", function(){
+    $(".navbar-ul").toggle();
+  })
+}
