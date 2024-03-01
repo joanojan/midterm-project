5c641c92422db98cf1832c36e0dbb6c7b12e37fc
Joan Vila Valls <joan.o.jan@gmail.com>
2024-03-01 19:22:32 +0100

    finished form validation with javascript

--- js/utils.js
+++ js/utils.js
@@ -1,12 +1,12 @@
 /**
  *          utils module
- *
  */
 
 const errorCallback = function (error) {
   console.error("Error: ", error);
 };
 
+//fetch the url, returns promise 
 async function getProjects() {
   const url =
     "https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects";
@@ -23,11 +23,16 @@ function removeSpaces(...words) {
   return words.map((word) => word.trim());
 }
 
-function validateEmail(mail) {
-  const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if (regex.test(mail)) return true;
+//simply check that the name is not empty
+function validateName(name) {
+  if (name) return true;
   return false;
 }
+
+function validateEmail (email) {
+  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  return regex.test(email);
+};
 
 function validatePhone(phone) {
   const regex = /^[0-9]{3}-[0-9]{3}-[0-9]{3}$/;
@@ -35,4 +40,9 @@ function validatePhone(phone) {
   return false;
 }
 
-export { getProjects, errorCallback, removeSpaces, validateEmail, validatePhone };
+function validateMessage(message) {
+  if (message.length < 10) return false;
+  return true;
+}
+
+export { getProjects, errorCallback, removeSpaces, validateName, validateEmail, validatePhone, validateMessage };
